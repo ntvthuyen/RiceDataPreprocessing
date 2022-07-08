f421a3b47da003f6a3f85ad71717849f1ea3f3ad
ntvthuyen <ntvthuyen@gmail.com>
2022-07-08 12:05:30 +0700

    modified train.py

--- exp/train.py
+++ exp/train.py
@@ -22,9 +22,28 @@ SEED=2484
 DEVICE=torch.device('cuda')
 pl.utilities.seed.seed_everything(SEED)
 
-net = FasterRCNNDetector(anno_dir='/home/lightkeima/Project/new_annotation/anno_train', image_dir='/home/lightkeima/Project/Lua/JPGimages/train', batch_size=3)
-trainer = pl.Trainer(max_epochs=5, accelerator=['cpu']
-#gpus=0
-, progress_bar_refresh_rate=100)
+train_transform = get_train_transform()
+test_transform = get_val_transform()
+
+net = FasterRCNNDetector(anno_dir=train_anno_path, image_dir=train_image_path, transform = train_transform, test_transform=test_transform, batch_size = 32)
+trainer = pl.Trainer(max_epochs=1, gpus=1, progress_bar_refresh_rate=100)
 trainer.fit(net)
 
+test_dataset = RiceDataset(test_anno_path, test_image_path, get_val_transform())
+test_data_loader = DataLoader(test_dataset, batch_size=4, shuffle=False, pin_memory=True, num_workers=4, collate_fn=collate_fn)
+
+detection_threshold = 0.5
+results = []
+net.model.to(DEVICE)
+net.model.eval()
+
+with torch.no_grad():
+    for images, image_ids in test_data_loader:
+        images = list(image.to(DEVICE) for image in images)
+        outputs = net.model(images)
+        print(image_ids)
+        for i, image in enumerate(images):
+            boxes = outputs[i]['boxes'].data.cpu().numpy()
+            labels = outputs[i]['labels'].data.cpu().numpy()
+            scores = outputs[i]['scores'].data.cpu().numpy()
+ 
